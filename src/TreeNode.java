20e7a463e1af06b6da5c4052cfea304765a37bb1
Vu <vuanhdora@gmail.com>
2024-12-07 02:26:42 +0700

    "Update"

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,7 +1,7 @@
 public class TreeNode {
     public int data;
-    TreeNode left;
-    TreeNode right;
+    public TreeNode left;
+    public TreeNode right;
 
     public TreeNode(int data) {
         this.data = 0;
