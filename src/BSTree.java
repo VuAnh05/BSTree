6032b9a7838604b877e32309e567c75301d6cdda
Vu <vuanhdora@gmail.com>
2024-12-06 10:30:25 +0700

    "Update"

--- src/BSTree.java
+++ src/BSTree.java
@@ -1,6 +1,9 @@
 public class BSTree {
-    private BSTree root;
+    private TreeNode root;
 
+    public void insert(int data){
+        root = insert(root, data);
+    }
     public TreeNode insert(TreeNode root, int data){
         if ( root == null){
             root = new TreeNode(data);
